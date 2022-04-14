13a7b2572cd81841f265c38a706aa4436b38120e
Ying Ying <yingyingfengg@gmail.com>
2022-04-14 11:13:28 -0400

    api notes route

--- app.js
+++ app.js
@@ -36,6 +36,21 @@ app.get('/api/purchases', async(req, res, next)=> {
   }
 });
 
+app.get('/api/notes', async(req, res, next) => {
+  try {
+    const user = await User.byToken(req.headers.authorization);
+    const note = await Note.findAll({
+      where: {
+        userId: user.id
+      }
+    })
+    res.send(note)
+  }
+  catch(ex) {
+    next(ex)
+  }
+})
+
 app.use((err, req, res, next)=> {
   console.log(err);
   res.status(err.status || 500).send({ error: err.message });
